aa4024a0f96cf146f721db10f12a96b0a1ce49f9
Aditya Agrawal <aditya.agrawal0309@gmail.com>
2021-10-06 20:56:27 +0530

    refactor

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { useData } from "../../context/DataContext/DataContext";
 
 export function Dashboard() {
@@ -5,13 +6,15 @@ export function Dashboard() {
     state: { dashboard },
   } = useData();
 
+  const navigate = useNavigate();
+
   return (
     <div className="flex justify-center">
       {dashboard.length ? (
         <div className="mt-8">
           {dashboard.map((item) => (
             <div key={item.quiz} className="mb-8">
-              <p className="text-center my-4 text-2xl uppercase"> {item.quiz} </p> 
+              <p className="text-center my-4 text-2xl uppercase">{item.quiz}</p>
               <div className="circle">
                 <p className="circle-text text-xl">{item.score} / 25</p>
               </div>
@@ -19,7 +22,12 @@ export function Dashboard() {
           ))}
         </div>
       ) : (
-        <p className="text-xl m-auto mt-8"> Play a quiz to view scores! </p>
+        <button
+          className="text-xl m-auto mt-8 bg-green-500 rounded-xl my-4 p-4"
+          onClick={() => navigate("/")}
+        >
+          Play a quiz to view scores!
+        </button>
       )}
     </div>
   );
